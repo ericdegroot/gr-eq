1ec4033335a00235776df95425a66293b1a92bee
Eric de Groot <eric@ericdegroot.com>
2013-08-07 12:20:42 -0700

    Initial work implementation for eq_lms_ff block.

--- lib/eq_lms_ff_impl.cc
+++ lib/eq_lms_ff_impl.cc
@@ -22,6 +22,8 @@
 #include "config.h"
 #endif
 
+// #include <cstdio>
+
 #include <gr_io_signature.h>
 #include "eq_lms_ff_impl.h"
 
@@ -62,9 +64,37 @@ namespace gr {
       const float *x = (const float *) input_items[0];
       const float *dn = (const float *) input_items[1];
       float *y = (float *) output_items[0];
-      
+
+      // fprintf(stderr, "work: noutput_items=%d, d_filter_size=%d, d_step_factor=%f\n", noutput_items, d_filter_size, d_step_factor);
+      // fprintf(stderr, "x[0]=%f, x[d_filter_size-1]=%f, x[noutput_items-1]=%f, x[(d_filter_size-1)+noutput_items-1]=%f\n", x[0], x[d_filter_size-1], x[noutput_items - 1], x[(d_filter_size - 1) + noutput_items - 1]);
+
+      float e;
       for (int i = 0; i < noutput_items; i++) {
-        y[i] = dn[i];
+        /*
+        for (int j = 0; j < d_filter_size; j++) {
+          fprintf(stderr, "\t%f", d_filter_taps[j]);
+        }
+        fprintf(stderr, "\n");
+
+        for (int j = 0; j < d_filter_size; j++) {
+          fprintf(stderr, "\t%f", x[d_filter_size - (j + 1) + i]);
+        }
+        fprintf(stderr, "\n");
+        */
+
+        y[i] = 0;
+        for (int j = 0; j < d_filter_size; j++) {
+          // fprintf(stderr, "\ti=%d, j=%d, d_filter_taps[j]=%f, x[%d]=%f\n", i, j, d_filter_taps[j], d_filter_size-(j+1)+i, x[d_filter_size - (j + 1) + i]);
+          y[i] += d_filter_taps[j] * x[d_filter_size - (j + 1) + i];
+        }
+
+        e = dn[i + (d_filter_size - 1)] - y[i];
+        // fprintf(stderr, "n=%d, dn(n)=%f, y(n)=%f, e(n)=%f\n", i, dn[i + (d_filter_size - 1)], y[i], e);
+
+        for (int j = 0; j < d_filter_size; j++) {
+          // fprintf(stderr, "\tj=%d, d_filter_taps[j]=%f\n", j, d_filter_taps[j]);
+          d_filter_taps[j] += 2 * d_step_factor * e * x[d_filter_size - (j + 1) + i];
+        }
       }
 
       // Tell runtime system how many output items we produced.
